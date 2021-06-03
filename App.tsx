67f6795d9336c78e235d7195eb8df033bdd7ce57
Basavachetan GM <chetan@revolotech.com>
2021-06-03 16:11:15 +0530

    Done some things - Amplify

--- App.tsx
+++ App.tsx
@@ -1,32 +1,84 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import {StatusBar} from 'expo-status-bar';
+import React, {useEffect} from 'react';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 
-import { withAuthenticator } from 'aws-amplify-react-native'
+import {withAuthenticator} from 'aws-amplify-react-native'
 
-import Amplify, { Auth } from 'aws-amplify'
+import Amplify, {Auth, API, graphqlOperation} from 'aws-amplify'
 import config from './src/aws-exports'
+
 Amplify.configure(config)
 Auth.configure(config);
 
-function App() {
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
+import {getUser} from './src/graphql/queries'
+import {createUser} from './src/graphql/mutations'
 
-  if (!isLoadingComplete) {
-    return null;
-  } else {
-    return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
-    );
-  }
+const randomImages = [
+    'https://hieumobile.com/wp-content/uploads/avatar-among-us-2.jpg',
+    'https://hieumobile.com/wp-content/uploads/avatar-among-us-3.jpg',
+    'https://hieumobile.com/wp-content/uploads/avatar-among-us-6.jpg',
+    'https://hieumobile.com/wp-content/uploads/avatar-among-us-9.jpg'
+]
+
+function App() {
+    const isLoadingComplete = useCachedResources();
+    const colorScheme = useColorScheme();
+
+    const getRandomImage = () => {
+        return randomImages[Math.floor(Math.random() * randomImages.length )]
+    }
+
+    // run this snippet only when app is first mounted
+    useEffect(() => {
+        const fetchUser = async () => {
+            // get Authenticated user from Auth
+            const userInfo = await Auth.currentAuthenticatedUser({bypassCache: true})
+
+            if (userInfo) {
+                // get the user from Backend with the user SUB from Auth
+                const userData = await API.graphql(graphqlOperation(getUser, {id: userInfo.attributes.sub}))
+
+                // if ( userData.data.getUser ) {
+                //     console.log("User is already registered in database")
+                //     return
+                // }
+
+                const newUser = {
+                    id: userInfo.attributes.sub,
+                    name: userInfo.username,
+                    imageUri: getRandomImage(),
+                    status: 'Hey, I am using Whatsapp'
+                }
+
+                console.log(newUser)
+
+                // If there is no user in DB with the id, the create one
+                await API.graphql(
+                    graphqlOperation(
+                        createUser,
+                        { input: newUser }
+                    )
+                )
+            }
+        }
+
+        fetchUser()
+    }, [])
+
+    if (!isLoadingComplete) {
+        return null;
+    } else {
+        return (
+            <SafeAreaProvider>
+                <Navigation colorScheme={colorScheme}/>
+                <StatusBar/>
+            </SafeAreaProvider>
+        );
+    }
 }
 
 export default withAuthenticator(App)
--- src/aws-exports.ts
+++ src/aws-exports.ts
@@ -7,7 +7,10 @@ const awsmobile = {
     "aws_cognito_region": "us-east-1",
     "aws_user_pools_id": "us-east-1_sy1tuWR20",
     "aws_user_pools_web_client_id": "4bp4sc668al9mfusl8hbcfkrhi",
-    "oauth": {}
+    "oauth": {},
+    "aws_appsync_graphqlEndpoint": "https://dfytlmbd2rabtaa2ehpimsklvi.appsync-api.us-east-1.amazonaws.com/graphql",
+    "aws_appsync_region": "us-east-1",
+    "aws_appsync_authenticationType": "AMAZON_COGNITO_USER_POOLS"
 };
 
 
